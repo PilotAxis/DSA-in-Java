96017814076a193223d242f782e751cb4ebeaaae
Mohammed Ahmed Majid <mohammedahmedmajid14@gmail.com>
2024-07-14 20:41:33 +0530

    Version 1.0 (Dated on July 14 2024)

---
+++ src/Primitives.java
@@ -0,0 +1,11 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int number = 10;
+        String letter = "Name";
+        char word = 'r';
+        float decimal = 3.14f;
+        long longNumber = 475987439735L;
+        double doubleNumber = 8454545.3863454;
+        boolean check = true;
+    }
+}
